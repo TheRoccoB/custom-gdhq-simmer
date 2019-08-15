bceba77c6278c87d55425b78dda7c41eeabf0cc4
rocco <roccob@gmail.com>
2019-08-14 23:51:23 -0700

    gamedevhq

--- customSite/customSite.js
+++ customSite/customSite.js
@@ -1,69 +1,64 @@
 (() => {
 
     window.customSite = {
-        title: 'ShareMyGame by GameDev.tv',
+        title: 'Start your game development career | GameDevHQ.com',
 
-        id: 'smg', //unique identifier
-        siteName: 'ShareMyGame.com',
+        id: 'gdhq', //unique identifier
+        siteName: 'GameDevHQ.com',
 
         theme_obj : {
-            primary: '#23a729',
-            secondary: '#b0bec5',
+            primary: '#2294A7',
+            secondary: '#FFA142',
             accent: '#8c9eff',
             error: '#b71c1c'
         },
 
-        howToYoutubeId_string: 'XWE9rzz3uV0',
-        longerHowToYoutubeId_string: 'ziiIlpjeMVY',
+        howToYoutubeId_string: 'vyDPTDQa4-I',
+        longerHowToYoutubeId_string: 'vyDPTDQa4-I',
 
         favicon_url: 'images/favicon.ico',
 
         // navbar_logo_url: '/images/gamedev.png',
-        navbar_logo_HTML: `<img src="{{baseUrl}}images/home_icon.svg" style="height: 25px;">`,
-        navbar_sitename_HTML: ' ',
-        navbar_style_object:{
-            'position':'relative',
-            'background-color': 'white',
-            'transform': 'translate(0,0)',
-            'left': 0
-        },
-        navbar_alwaysHideSpacer_bool:true,
-        navbar_showLowerOnFrontPage_bool: true,
+        navbar_logo_HTML: `<img src="{{baseUrl}}images/gamedevhq.png" style="height: 25px;">`,
+        navbar_sitename_HTML: 'gdhq',
+        // navbar_style_object:{
+        //     'position':'relative',
+        //     'background-color': 'white',
+        //     'transform': 'translate(0,0)',
+        //     'left': 0
+        // },
+        // navbar_alwaysHideSpacer_bool:true,
+        // navbar_showLowerOnFrontPage_bool: true,
 
-        beforeNavbar_html: '<div id="gdtv_header_inject" style="max-width:1440px;max-height:66px;margin-left:auto;margin-right:auto;"><div style="height:66px;background-color:#23A729;"></div></div></div>',
-
-        scriptsAfterAppMounted_array: [
-            'https://d1dewqfmv9j5d4.cloudfront.net/gamedevtv-header.js'
-        ],
-        stylesheetsBeforeAppMounted_array: [
-            'https://d1dewqfmv9j5d4.cloudfront.net/gamedevtv-header.css'
-        ],
+        // beforeNavbar_html: '<div id="gdtv_header_inject" style="max-width:1440px;max-height:66px;margin-left:auto;margin-right:auto;"><div style="height:66px;background-color:#23A729;"></div></div></div>',
 
         // hero
-        hero_background_color: '#071A32',
-        hero_background_url: '/images/smg_background.jpg',
-        hero_logo_url: '/images/gamedev.png',
+        hero_background_color: '#2294A7',
+        hero_background_url: '/images/gdhq-background.jpg',
+        hero_logo_url: '/images/gamedevhq.png',
         hero_logo_HTML: '<img style="width:125px;height:125px;" src="{{baseUrl}}/{{hero_logo_url}}">',
-        hero_title_HTML: `ShareMyGame<span style="font-size:34px">.com</span>`,
-        hero_description_HTML: `the simple way to share games from <a href="https://gamedev.tv" target="_blank" style="font-weight: bold;">GameDev.tv</a>!`,
+        hero_title_HTML: `Projects`,
+        hero_description_HTML: `turning your passion for Unity and game development into a career`,
 
         // sidebar
-        sidebar_description_HTML: `<p>ShareMyGame.com is a website by the <a href="https://gamedev.tv">GameDev.tv</a> team for featuring games created by our students!</p>
-    <p>ShareMyGame.com's technology is powered by <a href="https://simmer.io" style="font-weight:400">simmer.io</a>.</p>`,
+        sidebar_description_HTML: `
+            <p>Our goal is turning your passion for Unity and game development into a career.</p>
+            <p>Begin making your first game with our zombie FPS course, included with the free membership!</p>
+            <p>GamedevHQ's project site is powered by <a href="https://simmer.io" style="font-weight:400">simmer.io</a>.</p>`,
 
-        sidebar_social_obj:{
-            discord: 'https://discordapp.com/invite/eUSFZdJ',
-            twitter: 'https://twitter.com/GameDevTV',
-            youtube: 'https://www.youtube.com/channel/UC2CRYvS0FWRkTpCU3l4j8Mg',
-            facebook: 'https://www.facebook.com/GameDevTV',
-            medium: null,
-            contact: 'https://www.gamedev.tv/contact' //mailto: links also fine here.
-        },
+        // sidebar_social_obj:{
+        //     discord: 'https://discordapp.com/invite/eUSFZdJ',
+        //     twitter: 'https://twitter.com/GameDevTV',
+        //     youtube: 'https://www.youtube.com/channel/UC2CRYvS0FWRkTpCU3l4j8Mg',
+        //     facebook: 'https://www.facebook.com/GameDevTV',
+        //     medium: null,
+        //     contact: 'https://www.gamedev.tv/contact' //mailto: links also fine here.
+        // },
 
-        twitter_url: 'https://twitter.com/gamedevtv', //for twitter widget showing latest posts
+        twitter_url: 'https://twitter.com/gamedevhq', //for twitter widget showing latest posts
 
         // top and bottom linkbar (appfooter)
-        linkbar_accent_border:'1px solid #17A91D',
+        linkbar_accent_border:'1px solid #FFA142',
         linkbar_background_color:'#031403',
 
         // top linkbar
